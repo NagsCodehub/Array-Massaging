1d5f33e2433d48a6965de5f6a3c8cc18f6fd53c2
Karumanchi <karumann@ES.AD.ADP.COM>
2017-11-15 12:30:55 -0500

    base commit for aray massaging

---
+++ massagearray.js
@@ -0,0 +1,17 @@
+//keep on increasing the Array entries to see if it works for n numbers.
+var a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20];
+
+//got few approaches to do this and here are couple of sleek ones
+
+//with regular expressions
+var c= a.join().match(/\w?(\d+)*[24680]/g); 
+
+console.log(c)
+
+// with reduce
+var b = a.reduce(function(arr, num) { 
+    if (num % 2 === 0) arr.push(num); 
+    return arr; 
+}, []);
+
+console.log(b);
